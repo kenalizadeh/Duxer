3d01c4a3d96b1370bb8599ddd32cd76db56e5870
Kenan Alizadeh <kenalizadeh@Kenans-Mac-mini.local>
2025-11-29 19:54:46 +0400

    SPM

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "duxer",
-            sources: ["Duxer/Classes"]
+            path: "Duxer/Classes"
         )
     ]
 )
