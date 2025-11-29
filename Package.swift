0c46f982dac8515ed12dc6c6b19a4a685955535b
Kenan Alizadeh <kenalizadeh@Kenans-Mac-mini.local>
2025-11-29 19:53:14 +0400

    SPM

--- Package.swift
+++ Package.swift
@@ -1,18 +1,23 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-    name: "Duxer",
+    name: "duxer",
     products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "Duxer",
-            targets: ["Duxer"]
+            name: "duxer",
+            targets: ["duxer"]
         )
     ],
     targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Duxer",
-            sources: "Duxer/Classes"
+            name: "duxer",
+            sources: ["Duxer/Classes"]
         )
     ]
 )
